77f1acde572a33f6e242dc48e87098cc0f6596c6
vaniaileen <vaniaaileent@gmail.com>
2023-11-28 12:38:45 +0700

    Pertemuan tanggal 28 Nop 2023 2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -30,6 +30,8 @@
 			<td>{{ $p->pegawai_umur }}</td>
 			<td>{{ $p->pegawai_alamat }}</td>
 			<td>
+				<a href="/pegawai/view/{{ $p->pegawai_id }}" class="btn btn-success">View</a>
+				|
 				<a href="/pegawai/edit/{{ $p->pegawai_id }}" class="btn btn-warning">Edit</a>
 				|
 				<a href="/pegawai/hapus/{{ $p->pegawai_id }}"class="btn btn-danger">Hapus</a>
--- resources/views/tambah.blade.php
+++ resources/views/tambah.blade.php
@@ -1,9 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
-</head>
-<body>
+@extends('master')
+@section('title', 'Data Pegawai')
+@section('konten')
  
 	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
 	<h3>Data Pegawai</h3>
@@ -13,16 +10,18 @@
 	<br/>
 	<br/>
  
-	<form action="/pegawai/store" method="post">
+	<form action="/pegawai/store" method="post" class="form-horizontal">
 		{{ csrf_field() }}
+		<div class="form-group row">
+            <label for="nama" class="col-xs-3 col-form-label mr-2">Nama</label>
+            <div class="col-xs-9">
+            <input type="text" class="form-control" id="nama" name="nama">
+            </div>
+        </div>
 		Nama <input type="text" name="nama"> <br/>
 		Jabatan <input type="text" name="jabatan"> <br/>
 		Umur <input type="number" name="umur"> <br/>
 		Alamat <textarea name="alamat"></textarea> <br/>
 		<input type="submit" value="Simpan Data">
 	</form>
-		
- 
- 
-</body>
-</html>
+@endsection
